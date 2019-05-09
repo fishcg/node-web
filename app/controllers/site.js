2b2954de61374e83d2edca1727f4ab4a73d865f0
fishcg <353740902@qq.com>
2019-05-09 18:33:37 +0800

    draw-test

--- app/controllers/site.js
+++ app/controllers/site.js
@@ -14,6 +14,49 @@ var actions = {
             'description': '配置数据库以后，尝试使用 yoursite.com/news/index 访问动态页面 :）'
         }
         return data
+    },
+    draw: function () {
+        return {}
+    },
+    drawCard: function () {
+        let allNewCoupon = 0
+        let allRepeatCoupon = 0
+        let data = this.params.request.post
+        for (let i in data) {
+            data[i] = parseInt(data[i])
+        }
+        let length = data.ssrNum + data.srNum + data.rNum + data.nNum
+        let cards = []
+        for (let i = 1; i <= length; i++) {
+            let card = { id: i, repeateCoupon: 0 }
+            if (i <= data.ssrNum) {
+                card.repeateCoupon = data.ssrCoupon
+            } else if (i <= (data.srNum + data.ssrNum)) {
+                card.repeateCoupon = data.srCoupon
+            } else if (i <= (data.srNum + data.ssrNum + data.rNum)) {
+                card.repeateCoupon = data.rCoupon
+            } else {
+                card.repeateCoupon = data.nCoupon
+            }
+            cards.push(card)
+        }
+        let repeatCardIds = []
+        for (let i = 0; i < data.drawTimes; i++) {
+            let index = Math.floor((Math.random() * cards.length))
+            let card  = cards[index]
+            if (repeatCardIds.indexOf(card.id) === -1) {
+                allNewCoupon += data.newCoupon
+                repeatCardIds.push(card.id)
+            } else {
+                allRepeatCoupon += (data.newCoupon + card.repeateCoupon)
+            }
+        }
+        let allCoupon = allNewCoupon + allRepeatCoupon
+        return {
+            allCoupon: allCoupon,
+            allNewCoupon: allNewCoupon,
+            allRepeatCoupon: allRepeatCoupon,
+        }
     }
 }
 
