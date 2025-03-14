9a65e54485fb1c65bd83623e1a0a364dfaf3f352
fish <353740902@qq.com>
2025-03-14 20:43:27 +0800

    fix bug

--- app/models/Topic.js
+++ app/models/Topic.js
@@ -4,10 +4,10 @@ const baseModel = require(path.join(config.path.fish, 'mysql/baseModel'))
 
 class Topic extends baseModel.model {
   getTable() {
-    return 'lab_topic----------------5---------5---------5'
+    return 'lab_topic'
   }
   fields() {
-    let fields = ['id', 'title', 'cover', 'create_time', 'update_time', 'status', 'sort']
+    let fields = ['id', 'title', 'cover', 'create_time', 'update_time']
     return fields
   }
   attributeLabels() {
@@ -17,24 +17,6 @@ class Topic extends baseModel.model {
       cover: '专题封面',
       create_time: '创建时间',
       update_time: '修改时间',
-      status: '状态',
-      sort: '排序',
-    }
-  }
-
-  /**
-   * 通过ID查询专题
-   * @param {number} id 专题ID
-   * @returns {Promise<Object>} 专题信息
-   */
-  async findById(id) {
-    try {
-      return await this.find()
-        .select('id, title, cover, create_time, update_time, status')
-        .where('id = ?', [id])
-        .one()
-    } catch (error) {
-      throw new Error(`查询专题失败: ${error.message}`)
     }
   }
 }
